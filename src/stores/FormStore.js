9c986b0230ecab8580a7419977abc374be71c3be
Juwan Wheatley <datwheat@gmail.com>
2017-04-23 23:33:03 -0400

    adds persist

--- src/stores/FormStore.js
+++ src/stores/FormStore.js
@@ -1,11 +1,27 @@
 import { observable, action } from "mobx";
+import { create, persist } from "mobx-persist";
+import { AsyncStorage } from "react-native";
 
 class FormStore {
-  @observable firstLine = "";
-  @observable secondLine = "";
-  @observable town = "";
-  @observable postCode = "";
-  @observable groups = {
+  @persist
+  @observable
+  firstLine = "";
+
+  @persist
+  @observable
+  secondLine = "";
+
+  @persist
+  @observable
+  town = "";
+
+  @persist
+  @observable
+  postCode = "";
+
+  @persist("object")
+  @observable
+  groups = {
     Nursery: false,
     Reception: false,
     "Year 1": false,
@@ -21,6 +37,20 @@ class FormStore {
     "Year 11": false,
     "Year 12": false,
     "Year 13": false
+  };
+
+  constructor() {
+    this.hydrate();
+  }
+
+  @action hydrate = () => {
+    const pour = create({
+      storage: AsyncStorage
+    });
+
+    Object.keys(this).forEach(key => {
+      pour(key, this);
+    });
   };
 }
 
